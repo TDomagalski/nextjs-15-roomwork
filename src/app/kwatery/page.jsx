fe5f1e51f967df6c5b46f4bb27690d0d20898733
TDomagalski <tomekwebdev@gmail.comL>
2025-01-21 21:10:32 +0100

    feat: add svgs icons, kwatery layout loading, some changes

--- src/app/kwatery/page.jsx
+++ src/app/kwatery/page.jsx
@@ -1,10 +1,44 @@
-import '../globals.scss';
-import styles from './kwatery.module.scss';
+import Image from 'next/image';
 
-export default function Kwatery() {
+import RoomBedIcon from '../../../public/kwatery/room_equipment/RoomDoubleBedIcon';
+
+import '../globals.scss';
+import styles from './rooms.module.scss';
+import Link from 'next/link';
+
+export default function Rooms() {
   return (
-    <div className='container'>
-      <h1>Kwatery</h1>
-    </div>
+    <>
+      <section className={styles.row}>
+        <div className={`${styles.column} ${styles.column_cover}`}>
+          <Image alt='Image' src='/img-mini.webp' width={268} height={178} />
+        </div>
+        <div className={`${styles.column} ${styles.column_info}`}>
+          <div className={styles.info}>
+            <RoomBedIcon />
+            <span>Łóżka</span>
+          </div>
+          <div className={styles.info}>
+            <RoomBedIcon />
+            <span>Łóżka</span>
+          </div>
+          <div className={styles.info}>
+            <RoomBedIcon />
+            <span>Łazienka</span>
+          </div>
+        </div>
+        <Link
+          className={`${styles.column} ${styles.column_btns}`}
+          href='/kwatery/parter-1'
+        >
+          <button className={styles.btn_info}>
+            <span>Zdjęcia</span>
+          </button>
+          <button className={styles.btn_info}>
+            <span>Szczegóły</span>
+          </button>
+        </Link>
+      </section>
+    </>
   );
 }
