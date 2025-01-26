aa248f6b975792d95b1eca02d0a64b327c2f94ce
TDomagalski <tomekwebdev@gmail.comL>
2025-01-26 14:47:16 +0100

    feat: save

--- src/app/kwatery/page.jsx
+++ src/app/kwatery/page.jsx
@@ -21,7 +21,7 @@ export default function Rooms() {
         </div>
       </section>
 
-      <section className={styles.section}>
+      {/* <section className={styles.section}>
         <div className={styles.room}>
           <div className={`${styles.segment}, ${styles.segment_image}`}>
             <Image
@@ -65,139 +65,16 @@ export default function Rooms() {
             </button>
           </div>
         </div>
+      </section> */}
 
-        <div className={styles.room}>
-          <div className={`${styles.segment}, ${styles.segment_image}`}>
-            <Image
-              alt='Image'
-              src='/kwatery/img-mini.webp'
-              width={201}
-              height={134}
-            />
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_info}`}>
-            <div className={styles.info_icons}>
-              <div>
-                <RoomPersonIcon />
-                <span>3 - 4</span>
-              </div>
-              <div>
-                <RoomBedIcon />
-                <span>2</span>
-              </div>
-              <div>
-                <RoomDoubleBedIcon />
-                <span>1</span>
-              </div>
-              <div>
-                <RoomBathroomIcon />
-                <span>1</span>
-              </div>
-            </div>
-            <div className={styles.info_description}>
-              <h2>Kwatera 1</h2>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-                consequatur non accusantium cum natus ea molestias rerum
-                excepturi.
-              </p>
-            </div>
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_btn}`}>
-            <button className='infoButton'>
-              <span>Szczegóły</span>
-            </button>
-          </div>
-        </div>
-
-        <div className={styles.room}>
-          <div className={`${styles.segment}, ${styles.segment_image}`}>
-            <Image
-              alt='Image'
-              src='/kwatery/img-mini.webp'
-              width={201}
-              height={134}
-            />
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_info}`}>
-            <div className={styles.info_icons}>
-              <div>
-                <RoomPersonIcon />
-                <span>3 - 4</span>
-              </div>
-              <div>
-                <RoomBedIcon />
-                <span>2</span>
-              </div>
-              <div>
-                <RoomDoubleBedIcon />
-                <span>1</span>
-              </div>
-              <div>
-                <RoomBathroomIcon />
-                <span>1</span>
-              </div>
-            </div>
-            <div className={styles.info_description}>
-              <h2>Kwatera 1</h2>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-                consequatur non accusantium cum natus ea molestias rerum
-                excepturi.
-              </p>
-            </div>
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_btn}`}>
-            <button className='infoButton'>
-              <span>Szczegóły</span>
-            </button>
-          </div>
-        </div>
-
-        <div className={styles.room}>
-          <div className={`${styles.segment}, ${styles.segment_image}`}>
-            <Image
-              alt='Image'
-              src='/kwatery/img-mini.webp'
-              width={201}
-              height={134}
-            />
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_info}`}>
-            <div className={styles.info_icons}>
-              <div>
-                <RoomPersonIcon />
-                <span>3 - 4</span>
-              </div>
-              <div>
-                <RoomBedIcon />
-                <span>2</span>
-              </div>
-              <div>
-                <RoomDoubleBedIcon />
-                <span>1</span>
-              </div>
-              <div>
-                <RoomBathroomIcon />
-                <span>1</span>
-              </div>
-            </div>
-            <div className={styles.info_description}>
-              <h2>Kwatera 1</h2>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
-                consequatur non accusantium cum natus ea molestias rerum
-                excepturi.
-              </p>
-            </div>
-          </div>
-          <div className={`${styles.segment}, ${styles.segment_btn}`}>
-            <button className='infoButton'>
-              <span>Szczegóły</span>
-            </button>
-          </div>
-        </div>
-      </section>
+      <div className={styles.grid_container}>
+        <div className={styles.grid_item}>1</div>
+        <div className={styles.grid_item}>2</div>
+        <div className={styles.grid_item}>3</div>
+        <div className={styles.grid_item}>4</div>
+        <div className={styles.grid_item}>5</div>
+        <div className={styles.grid_item}>6</div>
+      </div>
     </>
   );
 }
