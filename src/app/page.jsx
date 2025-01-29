574a214e5321c27323da22fa2993ea209356acf1
TDomagalski <tomekwebdev@gmail.comL>
2025-01-29 12:05:00 +0100

    feat: add top-page segment with styles and rwd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <div className='container'>
-      <h1>Home</h1>
+      <div className='top_page'>
+        <div className='page_title'>
+          <div className='text'>
+            <h1>RoomWork - Kwatery pracownicze</h1>
+            {/* <p>krótki opis działalności</p> */}
+          </div>
+          <div className='box'></div>
+        </div>
+        <div className='page_img'>
+          <Image src='/global/img.webp' alt='img' width={100} height={100} />
+        </div>
+      </div>
     </div>
   );
 }
